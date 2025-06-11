105fb4e97f0632a3e6a4526a2cf6dcf2ad828d70
Slipstream <faxcluster20@protonmail.com>
2025-06-11 00:47:09 -0600

    fix(setup): update version to 1.0.0 and require Python 3.10 or higher

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='llm-git-commits',
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -24,5 +24,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.10',
 )
